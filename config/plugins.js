89fd3b3b853111041c2b35e4825e70a6047f37ff
Victor <llinaresvictor7@gmail.com>
2024-07-30 13:38:00 +0200

    sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY"),
+    },
+    settings: {
+      defaultFrom: "llinaresvictor7@gmail.com",
+      defaultReplyTo: "llinaresvictor7@gmail.com",
+    },
+  },
+});
